148f05b98f4d3103b6824e358f64bb921fb22116
yumad <yumad@runbox.com>
2017-01-04 10:58:55 -0800

    Elemental update 1/4
    Emergency Healing - Healing Surge lowest.health set in GUI for inCombat.
    outCombat
    * Healing Surge lowest.health <= 70 when not moving
    * Ghost Wolf after 2 seconds of continuous movement
    Keybinds
    * Liquid Magama Totem Keybind
    * !moving||moving added to each keybind to prevent weird recasting if moving/jumping
    Interrupts
    * !moving||moving added to prevent weird recasting if moving/jumping
    Lighting Rod Rotation
    * Rewrite to true rotation priority
    * Totem Mastery fix
    * Elemental Blast added for Fortified affix
    * !moving||moving added to prevent weird recasting if moving/jumping
    Icefury Rotation
    * Elemental Blast talent check
    '&' Off-GCD added to Astral Shift and Wind Shear

--- rotations/shaman/elemental.lua
+++ rotations/shaman/elemental.lua
@@ -4,7 +4,7 @@ local GUI = {
 	{type = 'checkbox', text = 'Enable Astral Shift', key = 'S_ASE', default = true},
 	{type = 'spinner', text = 'Astral Shift (Health %)', key = 'S_AS', default = 40},
 	{type = 'checkbox', text = 'Enable Healing Surge', key = 'S_HSGE', default = true},
-	{type = 'spinner', text = 'Healing Surge (Health %)', key = 'S_HSG', default = 30},
+	{type = 'spinner', text = 'Healing Surge (Health %)', key = 'S_HSG', default = 35},
 	{type = 'checkbox', text = 'Enable Earth Elemental', key = 'S_EEE', default = true},
 	{type = 'spinner', text = 'Earth Elemental (Health %)', key = 'S_EE', default = 50},
 	{type = 'checkbox', text = 'Enable Gift of the Naaru', key = 'S_GOTNE', default = true},
@@ -17,7 +17,7 @@ local GUI = {
 	
 	-- GUI Keybinds
 	{type = 'header', text = 'Keybinds', align = 'center'},
-	{type = 'checkbox', text = 'L-Shift: Totem Mastery', key = 'K_TM', default = true},
+	{type = 'checkbox', text = 'L-Shift: Liquid Magma Totem @ Cursor', key = 'K_LMT', default = true},
 	{type = 'checkbox', text = 'L-Control: Lightning Surge Totem @ Cursor', key = 'K_LST', default = true},
 	{type = 'checkbox', text = 'L-Alt: Earthbind Totem @ Cursor', key = 'K_ET', default = true},
 	{type = 'ruler'},{type = 'spacer'},
@@ -29,13 +29,19 @@ local GUI = {
 	{type = 'checkbox', text = 'Enable Bottom Trinket', key = 'Trinket_2', default = false},
 	{type = 'ruler'},{type = 'spacer'},
 
+	-- GUI Emergency Healing
+	{type = 'header', text = 'Emergency Healing', align = 'center'},
+	{type = 'checkbox', text = 'Enable Emergency Healing', key = 'E_EH', default = true},
+	{type = 'text', text = 'Thresholds set to ensure party member survival.'},
+	{type = 'spinner', text = 'Healing Surge (Health %)', key = 'E_HS', default = 35},
+	{type = 'ruler'},{type = 'spacer'},
 }
 
 local exeOnLoad = function()
 	-- Rotation loaded message.
 	print('|cff0068ff ----------------------------------------------------------------------|r')
 	print('|cff0068ff --- |rShaman: |cff0068ffELEMENTAL|r')
-	print('|cff0068ff --- |rLightning Rod (Mythic+) Talents: 1/3 - 2/1 - 3/1 - 4/2 - 5/2 - 6/1 - 7/2')
+	print('|cff0068ff --- |rLightning Rod (Mythic+) Talents: 1/3 - 2/1 - 3/1 - 4/2 - 5/2 or 5/3 (Fortified) - 6/1 - 7/2')
 	print('|cff0068ff --- |rIcefury Talents: 1/2 - 2/1 - 3/1 - 4/2 - 5/3 - 6/3 - 7/3')
 	print('|cff0068ff ----------------------------------------------------------------------|r')
 	print('|cffff0000 Configuration: |rRight-click the MasterToggle and go to Combat Routines Settings|r')
@@ -51,7 +57,7 @@ end
 
 local Survival = {
 	-- Astral Shift usage if enabled in UI.
-	{'Astral Shift', 'UI(S_ASE)&player.health<=UI(S_AS)'},
+	{'&Astral Shift', 'UI(S_ASE)&player.health<=UI(S_AS)'},
 	--Healing Surge usage if enabled in UI.
 	{'Healing Surge', 'UI(S_HSGE)&!moving&player.health<=UI(S_HSG)', 'player'},
 	-- Earth Elemental usage if enabled in UI.
@@ -65,12 +71,12 @@ local Survival = {
 }
 
 local Keybinds = {
-	-- Totem Mastery on Left-Shift if enabled in UI.
-	{'!Totem Mastery', 'keybind(lshift)&UI(K_TM)'},
+	-- Liquid Magma Totem at cursor on Left-Shift if enabled in UI.
+	{'!Liquid Magma Totem', '{!moving||moving}&talent(6,1)&keybind(lshift)&UI(K_LMT)', 'cursor.ground'},
 	-- Lightning Surge Totem at cursor on Left-Control if enabled in UI.
-	{'!Lightning Surge Totem', 'keybind(lcontrol)&UI(K_LST)', 'cursor.ground'},
+	{'!Lightning Surge Totem', '{!moving||moving}&keybind(lcontrol)&UI(K_LST)', 'cursor.ground'},
 	-- Earthbind Totem at cursor on Left-Alt if enabled in UI.
-	{'!Earthbind Totem', 'keybind(lalt)&UI(K_ET)', 'cursor.ground'},
+	{'!Earthbind Totem', '{!moving||moving}&keybind(lalt)&UI(K_ET)', 'cursor.ground'},
 }
 
 local Trinkets = {
@@ -80,46 +86,51 @@ local Trinkets = {
 	{'#trinket2', 'UI(trinket_2)'},
 }
 
+local Emergency = {
+	{'!Healing Surge', 'UI(E_EH)&lowest.health<=UI(E_HS)', 'lowest'},
+}
+
 local Interrupts = {
-	-- Wind Shear selected target.
-	{'!Wind Shear'},
+	{'&Wind Shear'},
+
 }
 
 local Dispel ={
-	-- Cleanse Spirit
 	{'%dispelall'},
 }
 
 -- Lighting Rod Rotation ##############################################################
 -- http://www.stormearthandlava.com/elemental-shaman-hub/lightning-rod-build-guide/
 local LRCooldowns = {
-	{'Totem Mastery', 'totem(Totem Mastery).duration<1'},
-	{'Stormkeeper'},
+	{{{'Totem Mastery', '{!moving||moving}&totem(Totem Mastery).duration<1||!player.buff(Tailwind Totem)||!player.buff(Storm Totem)||!player.buff(Resonance Totem)||!player.buff(Ember Totem)'},
 	{'Fire Elemental', '!talent(6,2)'},
-	{'Elemental Mastery', 'talent(6,1)'},
+	{'&Elemental Mastery', 'talent(6,1)'}, -- Remove when 7.1.5 is LIVE.
 	{'Blood Fury', 'player.buff(Elemental Mastery)'},
+	}, {'!moving||moving'}},
 }
 
 local LRAoE = {
-	{'Totem Mastery', 'totem(Totem Mastery).duration<1'},
-	{'Liquid Magma Totem', 'talent(6,1)', 'cursor.ground'},
-	{{{'Flame Shock', '!target.debuff(Flame Shock)'},
-	{'Lava Burst', 'player.buff(Lava Surge)'},
-	}, {'!moving||moving'}},
-	{'Earthquake', 'player.maelstrom>=50', 'cursor.ground'},
+	{'Totem Mastery', '{!moving||moving}&totem(Totem Mastery).duration<1||!player.buff(Tailwind Totem)||!player.buff(Storm Totem)||!player.buff(Resonance Totem)||!player.buff(Ember Totem)'},
+	{'Liquid Magma Totem', '{!moving||moving}&talent(6,1)', 'cursor.ground'},
+	{'Flame Shock', '{!moving||moving}&!target.debuff(Flame Shock)'},
+	{'Elemental Blast', 'talent(5,3)'},
+	{'Lava Burst', '{!moving||moving}&player.buff(Lava Surge)'},
 	{'Stormkeeper'},
+	{'Earthquake', '{!moving||moving}&player.maelstrom>=50', 'cursor.ground'},
 	{'Chain Lightning', nil, 'target'},
 }
 
 local LRST = {
-	{'Totem Mastery', 'totem(Totem Mastery).duration<1'},
-	{'Liquid Magma Totem', 'talent(6,1)', 'cursor.ground'},
-	{{{'Flame Shock', '!target.debuff(Flame Shock)||player.maelstrom>=20&player.buff(Elemental Focus)&target.debuff(Flame Shock).duration<9'},
-	{'Earth Shock', 'player.maelstrom>=92||player.maelstrom>=86&!player.buff(Lava Surge)'},
-	{'Lava Burst', 'target.debuff(Flame Shock).duration>spell(Lava Burst).casttime||player.buff(Lava Surge)||player.buff(Lava Surge)&player.buff(Stormkeeper).duration>spell(Lava Burst).casttime+gcd'},
-	}, {'!moving||moving'}},
+	{'Totem Mastery', '{!moving||moving}&totem(Totem Mastery).duration<1||!player.buff(Tailwind Totem)||!player.buff(Storm Totem)||!player.buff(Resonance Totem)||!player.buff(Ember Totem)'},
+	{'Flame Shock', '{!moving||moving}&{!target.debuff(Flame Shock)||player.maelstrom>=20&player.buff(Elemental Focus)&target.debuff(Flame Shock).duration<9}'},
+	{'Elemental Blast', 'talent(5,3)'},
+	{'Lava Burst', 'target.debuff(Flame Shock).duration>spell(Lava Burst).casttime'},
 	{'Stormkeeper'},
-	{'Chain Lightning', 'player.area(40).enemies>1'},
+	{'Liquid Magma Totem', '{!moving||moving}&talent(6,1)', 'cursor.ground'},
+	{'Earth Shock', '{!moving||moving}&player.maelstrom>=92'},
+	{'Lava Burst', '{!moving||moving}&player.buff(Lava Surge)'},
+	{'Earth Shock', '{!moving||moving}&player.maelstrom>=86&!player.buff(Lava Surge)'},
+	{'Lava Burst', 'player.buff(Stormkeeper).count<=3&player.buff(Stormkeeper).duration>spell(Lava Burst).casttime+gcd'},
 	{'Lightning Bolt', nil, 'target'},
 }
 
@@ -131,12 +142,9 @@ local IFCooldowns = {
 	{'Blood Fury', 'lastcast(Fire Elemental)'},
 }
 
-local IFAoE = {
-}
-
 local IFST = {
 	{{{'Flame Shock', '!target.debuff(Flame Shock)||player.maelstrom>=20&player.buff(Elemental Focus)&target.debuff(Flame Shock).duration<9'},
-	{'Elemental Blast', nil, 'target'},
+	{'Elemental Blast', 'talent(5,3)'},
 	{'Earth Shock', 'player.maelstrom>=92'},
 	{'Icefury', 'player.maelstrom<=76'},
 	{'Frost Shock', 'lastcast(Icefury)&spell(Frost Shock).charges=4||player.maelstrom>20'},
@@ -162,19 +170,21 @@ local inCombat = {
 	{Keybinds},
 	{Dispel, 'toggle(yuPS)&spell(Cleanse Spirit).cooldown=0'},
 	{Survival, 'player.health<100'},
+	{Emergency},
 	{Trinkets},
-	{Interrupts, 'toggle(interrupts)&target.interruptAt(70)&target.infront&target.range<=30'},
+	{Interrupts, '{!moving||moving}&toggle(interrupts)&target.interruptAt(70)&target.infront&target.range<=30'},
 	{LRCooldowns, 'talent(7,2)&toggle(cooldowns)'},
 	{IFCooldowns, 'talent(7,3)&toggle(cooldowns)'},
 	{LRAoE, 'talent(7,2)&toggle(aoe)&player.area(40).enemies>2'},
-	{IFAoE, 'talent(7,3)&toggle(aoe)&player.area(40).enemies>2'},
 	{LRST, 'talent(7,2)&target.infront&target.range<=40'},
 	{IFST, 'talent(7,3)&target.infront&target.range<=40'},
 }
 
 local outCombat = {
 	{Dispel, 'toggle(yuPS)&spell(Cleanse Spirit).cooldown=0'},
-	{Interrupts, 'toggle(interrupts)&target.interruptAt(70)&target.infront&target.range<=30'},
+	{Interrupts, '{!moving||moving}&toggle(interrupts)&target.interruptAt(70)&target.infront&target.range<=30'},
+	{'Healing Surge', '!moving&lowest.health<=70', 'lowest'},
+	{'Ghost Wolf', 'movingfor>=2&!player.buff(Ghost Wolf)'},
 }
 
 NeP.CR:Add(262, {
